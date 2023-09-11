2f27172192a59f60b135e40a5e66eb24ac179254
joshnwosu <joshuanwosu078@gmail.com>
2023-09-11 11:01:49 +0100

    update: root

--- src/renderer/src/screens/Home/Home.tsx
+++ src/renderer/src/screens/Home/Home.tsx
@@ -1,13 +1,15 @@
 import { Text } from '@mantine/core'
 import { Link, Outlet } from 'react-router-dom'
-
+// @ts-ignore
 import { Allotment } from 'allotment'
+import { SwitchToggle } from '@renderer/components/Widgets/SwitchTheme'
 
 const Home = () => {
   return (
     <div style={{ height: 400 }}>
       <Link to={'tracks'}>Go to 404</Link>
       <Text>Home</Text>
+      <SwitchToggle />
 
       {/* <Allotment vertical={false}>
         <Allotment.Pane minSize={100}>
