64d0250a8258204366808ee27fa6c486938584eb
jerry <jerrynwosu007@gmail.com>
2023-09-13 14:18:15 +0100

    fix tooltip wahala

--- src/renderer/src/components/Controls/PlayerControls/PlayerControls.tsx
+++ src/renderer/src/components/Controls/PlayerControls/PlayerControls.tsx
@@ -30,55 +30,49 @@ export default function PlayerControls() {
 
   return (
     <Flex className={classes.wrapper} direction={'column'} align={'center'}>
-      <Flex align={'center'} gap={'md'} mb={'sm'}>
-        <Tooltip label={shuffle ? 'Disable Shuffle' : 'Enable Shuffle'}>
-          <CustomIcon onClick={() => pcManager.shuffleTrack()}>
-            <TbArrowsShuffle color={shuffle ? '#1FDF64' : ''} size={iconSize} strokeWidth={2} />
-          </CustomIcon>
-        </Tooltip>
-        <Tooltip label={'Previous'}>
-          <CustomIcon onClick={() => pcManager.previousTrack()}>
-            <TbPlayerSkipBack size={iconSize} />
-          </CustomIcon>
-        </Tooltip>
-        <Tooltip label={'Step back'}>
-          <CustomIcon onClick={() => pcManager.stepBackward()}>
-            <TbPlayerTrackPrev size={iconSize} />
-          </CustomIcon>
-        </Tooltip>
-        <Tooltip label={playing ? 'Pause' : 'Play'}>
-          <CustomIcon onClick={() => pcManager.togglePlaying()} size={'xl'}>
-            {playing ? (
-              <TbPlayerPause size={'2rem'} strokeWidth={2} />
-            ) : (
-              <TbPlayerPlay size={'2rem'} strokeWidth={2} />
-            )}
-          </CustomIcon>
-        </Tooltip>
-        <Tooltip label={'Step forward'}>
-          <CustomIcon onClick={() => pcManager.previousTrack()}>
-            <TbPlayerTrackNext size={iconSize} />
-          </CustomIcon>
-        </Tooltip>
-        <Tooltip label={'Nest'}>
-          <CustomIcon onClick={() => pcManager.stepBackward()}>
-            <TbPlayerSkipForward size={iconSize} />
-          </CustomIcon>
-        </Tooltip>
-        <Tooltip label={`Repeat ${repeat}`}>
-          <CustomIcon onClick={() => pcManager.repeatTrack()}>
-            {repeat !== RepeatEnum.One && (
-              <TbRepeat
-                size={iconSize}
-                color={repeat !== RepeatEnum.Off ? '#1FDF64' : ''}
-                strokeWidth={2}
-              />
-            )}
-            {repeat === RepeatEnum.One && (
-              <TbRepeatOnce size={iconSize} color={'#1FDF64'} strokeWidth={2} />
-            )}
-          </CustomIcon>
-        </Tooltip>
+      <Flex align={'center'} gap={'md'} mb={4}>
+        <CustomIcon
+          onClick={() => pcManager.shuffleTrack()}
+          label={shuffle ? 'Disable Shuffle' : 'Enable Shuffle'}
+        >
+          <TbArrowsShuffle color={shuffle ? '#1FDF64' : ''} size={iconSize} strokeWidth={2} />
+        </CustomIcon>
+        <CustomIcon onClick={() => pcManager.previousTrack()} label={'Previous'}>
+          <TbPlayerSkipBack size={iconSize} />
+        </CustomIcon>
+        <CustomIcon onClick={() => pcManager.stepBackward()} label={'Step back'}>
+          <TbPlayerTrackPrev size={iconSize} />
+        </CustomIcon>
+
+        <CustomIcon
+          onClick={() => pcManager.togglePlaying()}
+          size={'xl'}
+          label={playing ? 'Pause' : 'Play'}
+        >
+          {playing ? (
+            <TbPlayerPause size={'2rem'} strokeWidth={2} />
+          ) : (
+            <TbPlayerPlay size={'2rem'} strokeWidth={2} />
+          )}
+        </CustomIcon>
+        <CustomIcon onClick={() => pcManager.previousTrack()} label={'Step forward'}>
+          <TbPlayerTrackNext size={iconSize} />
+        </CustomIcon>
+        <CustomIcon onClick={() => pcManager.stepBackward()} label={'Nest'}>
+          <TbPlayerSkipForward size={iconSize} />
+        </CustomIcon>
+        <CustomIcon onClick={() => pcManager.repeatTrack()} label={`Repeat ${repeat}`}>
+          {repeat !== RepeatEnum.One && (
+            <TbRepeat
+              size={iconSize}
+              color={repeat !== RepeatEnum.Off ? '#1FDF64' : ''}
+              strokeWidth={2}
+            />
+          )}
+          {repeat === RepeatEnum.One && (
+            <TbRepeatOnce size={iconSize} color={'#1FDF64'} strokeWidth={2} />
+          )}
+        </CustomIcon>
       </Flex>
       <Seek />
     </Flex>
@@ -86,15 +80,18 @@ export default function PlayerControls() {
 }
 
 interface CustomIconProps {
-  children: React.ReactNode
+  children: React.ReactNode | any
   size?: MantineSize | number
   onClick?: () => void
+  label?: string
 }
 
-function CustomIcon({ children, size, onClick }: CustomIconProps) {
+function CustomIcon({ children, size, onClick, label }: CustomIconProps) {
   return (
-    <ActionIcon size={size || 'lg'} onClick={onClick} variant={'transparent'} radius={'xl'}>
-      {children}
-    </ActionIcon>
+    <Tooltip label={label}>
+      <ActionIcon size={size || 'lg'} onClick={onClick} variant={'transparent'} radius={'xl'}>
+        {children}
+      </ActionIcon>
+    </Tooltip>
   )
 }
